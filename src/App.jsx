200e9b8ea9754027b425bd24dc9e189e25b445ad
Muhammad Rameez <buildwithrameez@gmail.com>
2026-09-25 19:25:01 +0500

    Pokemon project base setup complete

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import { Pokemon } from './Components/pokemon.jsx'
+
+function App() {
+  return<>
+      <Pokemon />
+  </>
+}
+
+export default App
---
+++ src/Components/pokemon.jsx
@@ -0,0 +1,7 @@
+import '../Components/pokemon.css'
+
+export const Pokemon = () => {
+    return<>
+        <h1>Pokemon</h1>
+    </>
+};
